1a690ce7f1551bade16d1fcedf9ca011c1173f8b
Shubham Mukhopadhyay <117881946+ShubhamMukhopadhyay@users.noreply.github.com>
2026-04-27 10:17:32 +0530

    Add files via upload

---
+++ turtle-crossing-start/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self, shape = "classic", undobuffersize = 1000, visible = True):
+        super().__init__(shape, undobuffersize, visible)
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(x=-280, y=250)
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align= "left", font=FONT)
+
+    def increase_level (self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(x=0, y=0)
+        self.write(f"GAME OVER", align= "center", font=FONT)
